fcc8bbf960ee822d764e0925f4b500761fb71ea6
j-alleva <jacka303030@outlook.com>
2026-02-06 18:15:13 -0800

    feat: add missing date dimension

--- sql/queries/q3_high_winds.sql
+++ sql/queries/q3_high_winds.sql
@@ -3,10 +3,12 @@
 
 SELECT 
     l.location_name,
+    d.date_value,
     f.hour,
     f.wind_speed_10m,
     f.temperature_2m
 FROM fact_weather_hourly f
 JOIN dim_location l ON f.location_id = l.location_id
+JOIN dim_date d ON f.date_id = d.date_id
 WHERE f.wind_speed_10m > 15
 ORDER BY f.wind_speed_10m DESC;
